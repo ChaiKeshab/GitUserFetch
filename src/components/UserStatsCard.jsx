f680046be734ee21f69d1f23150d7455ff9b3ced
Chai Keshab <keshab3886@gmail.com>
2023-10-19 22:21:42 +0545

    width fix

--- src/components/UserStatsCard.jsx
+++ src/components/UserStatsCard.jsx
@@ -1,7 +1,7 @@
 /*eslint-disable */
 const UserStatsCard = ({ number, label }) => {
     return (
-        <div className='flex w-max py-2 px-4 m-1 bg-[#EAEAEA] rounded-md justify-center
+        <div className='flex w-1/3 min-w-fit py-2 px-4 m-1 bg-[#EAEAEA] rounded-md justify-center
         md:block md:space-y-3 md:w-fit md:bg-white md:p-0 md:m-0'>
 
             <div className='md:text-4xl mr-1 font-bold md:font-normal'>{number}</div>
